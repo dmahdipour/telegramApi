a3959ef3eacc395c06ed5771603e4d85b5dd7281
dmehdipour <daruosh.mehdipour@gmail.com>
2021-09-27 13:02:16 +0330

    Define padidegoharMsg

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ def sendMessage(id):
     _result={'status':'ok', 'message':str(_resultOfTel)}
     return jsonify(_result)
 
+@app.route("/padidegoharMsg/<Name>/<Phone>/<Title>/")
+def padidegoharMsg(Name,Phone,Title):
+
+    _url="https://api.telegram.org/bot233935395:AAGlA52RnM74d_H3HKCaj4RBi37Oah5FqAc/sendMessage?chat_id=-1001141238059&parse_mode=HTML&text=کارآموز جدیدی در سایت ثبت نام کرده است:\n"+Name+"\n"+Phone+"\n"+Title
+    _resultOfTel=requests.get(_url)
+    _result={'status':'ok', 'message':str(_resultOfTel)}
+    return jsonify(_result)
+
 if __name__ == '__main__':
     app.run(debug=True, port=8090)
     
